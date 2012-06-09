7a7888c924a425cd759f920b807850039daccc07
Hunter Barrington <hunter@ftwdev.com>
2012-06-09 18:32:38 -0400

    refactored EVERYTHING! router is now in server and takes controllers, and gets passed to a client handler. StuffedPackets ferry data via serializable. authentication is done

---
+++ fuzzy-lana-server/src/com/ftwdev/stuffinc/server/RetardedRouter.java
@@ -0,0 +1,30 @@
+package com.ftwdev.stuffinc.server;
+
+import com.ftwdev.stuffinc.core.User;
+
+public class RetardedRouter {
+    private static final int WAITING = 0;
+    
+    
+    private int state = WAITING; 
+    
+    
+    //controllers
+    UserController uc = null;
+    
+    
+    public RetardedRouter(UserController uc){
+    	this.uc = uc;
+    }
+    
+    public StuffedPacket processInput(StuffedPacket in) {
+    	StuffedPacket out = null;
+    	
+        if (state == WAITING) {
+        	if ((in.message).equals("AUTHENTICATING")){
+        		out = uc.authenticate((User) in.payload);
+        	}
+        } 
+        return out;
+    }
+}
