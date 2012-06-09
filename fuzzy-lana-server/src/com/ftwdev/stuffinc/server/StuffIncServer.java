5811eec9c4a86dc512350da3b8af8b07defae312
Hunter Barrington <hunter@ftwdev.com>
2012-06-08 23:01:03 -0400

    started sockets for server

--- fuzzy-lana-server/src/com/ftwdev/stuffinc/server/StuffIncServer.java
+++ fuzzy-lana-server/src/com/ftwdev/stuffinc/server/StuffIncServer.java
@@ -1,5 +1,25 @@
 package com.ftwdev.stuffinc.server;
 
-public class StuffIncServer {
+import java.net.Socket;
 
+public class StuffIncServer {
+    public static void main(String[] args){
+
+        //sockets for accepting connections
+        try {
+            stuffServer = new ServerSocket(12345);
+        }
+        catch (IOException e){
+            System.out.println("Could not bind to port 12345");
+            System.exit(-1);
+        }
+        Socket clientSocket = null;
+        try {
+            clientSocket = stuffServer.accept();
+        }
+        catch (IOException e){
+            System.out.println("Could not accept client connections");
+            System.ext(-1);
+        }
+    }
 }
