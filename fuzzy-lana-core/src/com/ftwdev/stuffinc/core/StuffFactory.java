ca700591e81ec7a87d835bac73e53186ea72e8bd
Hunter Barrington <hunter@ftwdev.com>
2012-06-10 12:59:26 -0400

    started adding wild

--- fuzzy-lana-core/src/com/ftwdev/stuffinc/core/StuffFactory.java
+++ fuzzy-lana-core/src/com/ftwdev/stuffinc/core/StuffFactory.java
@@ -4,8 +4,8 @@ import java.util.Random;
 
 public class StuffFactory {
 
-	//private static final String[] STARTERS = {"Explosive Soda Can", "Playful Pogo", "Three-Legged Stepstool"};
-	private static final String[] STARTERS = {"stuff1", "stuff2"};
+	private static final String[] STARTERS = {"Explosive Soda Can", "Playful Pogo", "Three-Legged Stepstool"};
+	//private static final String[] STARTERS = {"stuff1", "stuff2"};
 	public static Thing getStarter() {
 		Random random = new Random();
 		Stuff stuff = Stuff.getStuffList().get(STARTERS[random.nextInt(STARTERS.length)]);
@@ -21,5 +21,29 @@ public class StuffFactory {
 			}
 		}
 		return starter;
+	}
+	
+	public static Thing getWild(int rarity, int level) {
+		Random random = new Random();
+		Stuff stuff = Stuff.getStuffList().get();
+		
+		//level up
+		Thing wild = new Thing(stuff);
+		for(int i; i<level; i++){
+			wild.levelUp();
+		}
+		
+		
+		//get moves
+		for(int moveLevel : stuff.getMoveList().keySet()) {
+			if(moveLevel <= wild.getLevel()) {
+				if(wild.getMoveSet().size() >= 4) {
+					wild.getMoveSet().add(random.nextInt(4), stuff.getMoveList().get(moveLevel));
+				} else {
+					wild.getMoveSet().add(stuff.getMoveList().get(moveLevel));
+				}
+			}
+		}
+		return wild;
 	}
 }
