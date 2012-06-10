aee40564f6a674229e7f3aed6daacd650c745544
Samuel Hu <sam@barringtonweb.com>
2012-06-10 14:48:27 -0400

    Added stuff factory test

--- fuzzy-lana-core/test/com/ftwdev/stuffinc/core/test/StuffFactoryTest.java
+++ fuzzy-lana-core/test/com/ftwdev/stuffinc/core/test/StuffFactoryTest.java
@@ -32,4 +32,24 @@ public class StuffFactoryTest {
 		}
 	}
 	
+	@Test
+	public void testGetWild() throws FileNotFoundException {
+		Move.initMoveList(new FileInputStream("test/assets/moves.yml"));
+		Stuff.initStuffList(new FileInputStream("test/assets/stuff.yml"));
+		Thing starter = StuffFactory.getWild();
+		System.out.println(starter.getName() + " - lvl " + starter.getLevel());
+		System.out.println("\tType: " + starter.getType());
+		System.out.println("\tHealth: "+starter.getHealth());
+		System.out.println("\tForce: "+starter.getForce());
+		System.out.println("\tHardiness: "+starter.getHardiness());
+		System.out.println("\tInsight: "+starter.getInsight());
+		System.out.println("\tWillpower: "+starter.getWillpower());
+		System.out.println("\tMobility: "+starter.getMobility());
+		System.out.println("\tSoul: "+starter.getSoul());
+		System.out.println("\tMoves:");
+		for(Move m : starter.getMoveSet()) {
+			System.out.println("\t\t" + m.getName());
+		}
+	}
+	
 }
